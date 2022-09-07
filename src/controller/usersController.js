1c4b259f2950f0480a2a24d364c88f5187bd84eb
Marco <marcoanbjr@gmail.com>
2022-09-07 20:56:30 -0300

    hide password

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -3,18 +3,22 @@ import users from './../models/User.js';
 export default class UserController {
 
     static listUsers = (req, res) => {
-        users.find((err, users) => {
-            res.status(200).json(users)
+        users.find((err, user) => {
+            user = user.map((user) => {
+                const userPassword = {...user['_doc']};
+                delete userPassword.password;
+                return userPassword;
+            })
+            res.status(200).json(user)
         })
     }
 
     static userById = (req, res) => {
         const {id} = req.params;
 
-        users.findById(id, (err, users) => {
-            
+        users.findById(id, (err, users) => {            
             err ?
-                res.status(400).send({message: `${err.message} - id incorreto !`}) :
+                res.status(404).send({message: `${err.message} - id incorreto !`}) :
                 res.status(200).send(users)
         })
     }
@@ -36,7 +40,7 @@ export default class UserController {
 
         users.findByIdAndUpdate(id, {$set: req.body}, (err) => {
             if(err) {
-                res.status(500).send({message})
+                res.status(404).send({message})
             } else {
                 res.status(200).send({message: 'Usuário cadastrado !'})
             }
@@ -51,7 +55,7 @@ export default class UserController {
 
         users.findByIdAndDelete(id, (err) => {
             if(err) {
-                res.status(500).send({message: err.message})
+                res.status(404).send({message: err.message})
             } else {
                 res.status(204).send({message: 'Usuário removido !'})
             }
